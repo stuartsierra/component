f470e30d1ca3bde385cfdab557910bb38e055434
Stuart Sierra <mail@stuartsierra.com>
2013-10-25 09:35:35 -0400

    project.clj: set warn-on-reflection and min-lein-version

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/stuartsierra/component"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :min-lein-version "2.1.3"  ; added :global-vars
   :dependencies [[com.stuartsierra/dependency "0.1.1"]]
+  :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}
