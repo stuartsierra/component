ec0193329e8020b55d85278c138b5b4f42b8cbc7
Howard M. Lewis Ship <hlship@walmartlabs.com>
2015-11-10 13:37:37 -0800

    Add missing dev dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/dependency "0.2.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+                                  [org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}})
