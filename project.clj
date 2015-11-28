2591eaff8878b05585e2dca97e83895ce3be87d7
Stuart Sierra <mail@stuartsierra.com>
2015-11-28 13:47:47 -0500

    Add Clojure 1.7 as explicit dependency
    Since version 0.3.0 depends on conditional read support in Clojure 1.7,
    it is probably a good idea to have a minimum Clojure version declared as
    a dependency.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/stuartsierra/component"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[com.stuartsierra/dependency "0.2.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [org.clojure/tools.namespace "0.3.0-alpha1"]]
+  :dependencies [[com.stuartsierra/dependency "0.2.0"]
+                 [org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha1"]]
                    :source-paths ["dev"]}})
