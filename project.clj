c32b823c6b948b0dce37ec13a8896099c7b4fe7d
Stuart Sierra <mail@stuartsierra.com>
2015-11-28 13:59:24 -0500

    Prepare project.clj for 0.3.2-SNAPSHOT development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stuartsierra/component "0.3.1"
+(defproject com.stuartsierra/component "0.3.2-SNAPSHOT"
   :description "Managed lifecycle of stateful objects"
   :url "https://github.com/stuartsierra/component"
   :license {:name "The MIT License"
