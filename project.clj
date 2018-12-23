4e966d55f13d77b8aedc5077fdd9ed8334a7693f
Joe Lane <jplane.tech@gmail.com>
2018-12-23 14:23:48 -0600

    add test alias for clojure 1.7 through 1.10

--- project.clj
+++ project.clj
@@ -5,5 +5,10 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/dependency "0.2.0"]
                  [org.clojure/clojure "1.7.0"]]
+  :aliases {"test-all" ["with-profile" "+1.7:+1.8:+1.9:+1.10" "test"]}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha1"]]
-                   :source-paths ["dev"]}})
+                   :source-paths ["dev"]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}})
