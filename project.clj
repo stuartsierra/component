5af4ad06fdc3ff3240573ae9394da92d8cf90c7e
Stuart Sierra <mail@stuartsierra.com>
2019-07-27 12:00:25 -0400

    Make Clojure dependency scope 'provided'
    The Component library since version 0.3.0
    requires Clojure 1.7.0 or later,
    which is why it was added as a direct dependency
    in commit 2591eaff8878b05585e2dca97e83895ce3be87d7
    
    If a project using Component declares a dependency
    on a later Clojure version, most Maven-based tools
    will silently upgrade to the later version.
    However, some tools will print a warning in this case,
    as reported on GitHub.
    
    Reported by https://github.com/daveyarwood
    as https://github.com/stuartsierra/component/issues/65
    
    Fixes #65

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/dependency "0.2.0"]
-                 [org.clojure/clojure "1.7.0"]]
+                 [org.clojure/clojure "1.7.0" :scope "provided"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha1"]]
                    :source-paths ["dev"]}})
