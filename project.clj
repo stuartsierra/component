9f9653d1d95644e3c30beadf8c8811f86758ea23
Stuart Sierra <mail@stuartsierra.com>
2020-03-07 11:51:48 -0500

    Set release version 1.0.0 in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stuartsierra/component "0.4.1"
+(defproject com.stuartsierra/component "1.0.0"
   :description "Managed lifecycle of stateful objects"
   :url "https://github.com/stuartsierra/component"
   :license {:name "The MIT License"
