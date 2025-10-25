98b17aba9695972f6375d2ed42cafdfa52973f1d
Alessandra Sierra <105090644+lambdasierra@users.noreply.github.com>
2025-10-25 12:04:33 -0400

    project.clj: set release version to 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.stuartsierra/component "1.1.1-SNAPSHOT"
+(defproject com.stuartsierra/component "1.2.0"
   :description "Managed lifecycle of stateful objects"
   :url "https://github.com/stuartsierra/component"
   :license {:name "The MIT License"
