60dcfb5b7796743e7f563eb5c9ed960f8a474122
Łukasz Korecki <lukasz@korecki.me>
2025-11-01 08:48:10 -0700

    Make 'system-map' work with kv paris or a map
    This requires bumping min Clojure version to 1.11

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/dependency "1.0.0"]
-                 [org.clojure/clojure "1.7.0" :scope "provided"]])
+                 [org.clojure/clojure "1.11.0" :scope "provided"]])
